0a78c2a461036fc135debea9003e1b804aba6542
Kilex <120309721+CrazyDoctor56@users.noreply.github.com>
2024-11-17 11:58:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt6.QtWidgets import (QMainWindow, QApplication)
+from ui import Ui_MainWindow
+
+app = QApplication([])
+win = QMainWindow()
+ui = Ui_MainWindow()
+
+ui.setupUi(win)
+
+def func():
+    text = "n"
+    ui.result.setText(text)
+
+ui.generate.clicked.connect(func)
+
+
+win.show()
+app.exec()
